b5db06bb82bd6e5bcc346fbf78d5ca2392f4e86e
7usaa <olszewskistasio1@gmail.com>
2025-02-10 20:59:27 +0100

    Add files via upload

---
+++ pinger by 7usa.bat
@@ -0,0 +1,18 @@
+@echo off
+color 4
+
+title @7usaa youtube
+
+:greeting
+cls
+
+
+ECHO SUICIDE UWU
+
+set /p IP=Enter IP::
+:top
+PING -n 1 %IP% | FIND "TTL="
+IF ERRORLEVEL 1 (SET in=0b & echo Downed.) 
+color %in%
+ping -t 2 0 10 127.0.0.1 >nul
+GoTo top 
