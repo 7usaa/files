d70f7a1e97557ec06662493c2e38f3b35af3d593
7usaa <olszewskistasio1@gmail.com>
2025-02-10 21:12:46 +0100

    Update pinger by 7usa.bat

--- pinger by 7usa.bat
+++ pinger by 7usa.bat
@@ -12,7 +12,7 @@ ECHO SUICIDE UWU
 set /p IP=Enter IP::
 :top
 PING -n 1 %IP% | FIND "TTL="
-IF ERRORLEVEL 1 (SET in=0b & echo Downed.) 
+IF ERRORLEVEL 1 (SET in=0b & echo Fucked like hoe.) 
 color %in%
 ping -t 2 0 10 127.0.0.1 >nul
 GoTo top 
